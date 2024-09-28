1c6add498cd17eb6dd8c8401e42459b2270cf1f3
Dmitry <zweidmitr@gmail.com>
2024-09-28 10:41:28 +0300

    t002 - изменения для коммита

--- src/main/java/ru/job4j/repository/AchievementRepository.java
+++ src/main/java/ru/job4j/repository/AchievementRepository.java
@@ -1,7 +1,7 @@
 package ru.job4j.repository;
 
 /**
- * класс для хранения данных о достижениях пользователя.
+ * Класс для хранения данных о достижениях пользователя.
  */
 public class AchievementRepository {
 }
